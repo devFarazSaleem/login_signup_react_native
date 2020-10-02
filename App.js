87880e70fad04925a1c1f0669c3b129f89eae9c8
= <=>
2020-10-02 09:27:58 +0500

    stuck when authing

--- App.js
+++ App.js
@@ -1,19 +1,29 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet } from "react-native";
+import { AsyncStorage, StyleSheet } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import SignUp from "./screens/SignUp";
 import Login from "./screens/login";
 import { NavigationContainer } from "@react-navigation/native";
+import Home from "./screens/Home";
 
 const Stack = createStackNavigator();
 export default function App() {
+  const loggedIN = AsyncStorage.getItem("loggedin");
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="SignUp" component={SignUp} />
-        <Stack.Screen name="Login" component={Login} />
-      </Stack.Navigator>
+      {loggedIN ? (
+        <Stack.Navigator>
+          <Stack.Screen name="SignUp" component={SignUp} />
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+      ) : (
+        <Stack.Navigator>
+          <Stack.Screen name="SignUp" component={SignUp} />
+          <Stack.Screen name="Login" component={Login} />
+        </Stack.Navigator>
+      )}
     </NavigationContainer>
   );
 }
